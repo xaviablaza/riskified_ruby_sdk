055c91a906dcd40ecb30b0e0c49081a8c0b1c2f0
Xavi Ablaza <25378966+xaviablaza@users.noreply.github.com>
2022-03-16 08:28:38 +0800

    Initial commit

---
+++ spec/riskified_ruby_sdk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RiskifiedRubySdk do
+  it "has a version number" do
+    expect(RiskifiedRubySdk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
